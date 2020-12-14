e51b6240a9c5fd79555b3b8d0b1c39c52e6f700e
dennergazevedo <dennergazevedo@gmail.com>
2020-12-14 18:01:36 -0300

    REPAIR URL SUCCESS

--- src/page/Success/index.tsx
+++ src/page/Success/index.tsx
@@ -1,15 +1,36 @@
+/* eslint-disable no-console */
 /* eslint-disable react/jsx-one-expression-per-line */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // STYLED COMPONENTS
 import { Container, Body, Logomarca, Form, Background } from './styles';
 
 const ForgotPass: React.FC = () => {
+  const [email, setEmail] = useState<unknown>('');
   const { width } = window.screen;
 
   function handleContact() {
     window.location.href = '/contact';
   }
+
+  function handleEmail() {
+    let url: string | Array<string> = window.location.href.split(
+      'customer_email=',
+    );
+    if (!url[1]) window.location.href = '/';
+    url = url[1].split('&');
+    if (!url[0]) window.location.href = '/';
+    url = url[0].replace('%40', '@');
+    if (!url) {
+      window.location.href = '/';
+    } else {
+      setEmail(url);
+    }
+  }
+
+  useEffect(() => {
+    handleEmail();
+  }, []);
 
   return (
     <Container>
@@ -24,8 +45,8 @@ const ForgotPass: React.FC = () => {
             Você concluiu o seu pedido.
           </b>
           <br />
-          Caso você tenha escolhido a opção de <br />
-          Boleto Bancário nós já enviamos no seu e-mail! ;)
+          Os dados de pagamento foram enviados <br />
+          para o e-mail <b>{`${email}`}</b>
         </span>
         <br />
         <span>
@@ -33,7 +54,11 @@ const ForgotPass: React.FC = () => {
           contato conosco clicando no botão abaixo.
         </span>
         <Form>
-          <button type="button" onClick={handleContact} className="signIn">
+          <button
+            type="button"
+            onClick={() => handleContact()}
+            className="signIn"
+          >
             TIRE SUAS DÚVIDAS
           </button>
           <a href="/login" className="signUp">

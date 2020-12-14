8c278ea1858166cf2930afec9b7de2537df4e610
dennergazevedo <dennergazevedo@gmail.com>
2020-12-14 16:55:33 -0300

    ADD GOOGLE ANALYTICS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import Routes from './routes';
 
 /** Global Styles */
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
+  function initializeReactGoogleAnalytics() {
+    ReactGA.initialize('G-EDGREG5ZLQ');
+    ReactGA.pageview('/');
+  }
+
+  initializeReactGoogleAnalytics();
   return (
     <>
       <GlobalStyles />
